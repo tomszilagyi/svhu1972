c072c1609bcc620ce6166e0c7d852800ae4f762b
Tom Szilagyi <tomszilagyi@gmail.com>
2016-12-29 23:35:59 +0100

    Generate correctly sized app icons via script

---
+++ imagery/06_mk_icons.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+opts="-transparent white -define png:compression-level=9 -define png:exclude-chunk=all"
+res="../app/src/main/res"
+
+function conv {
+    mkdir -p $res/$1
+    convert cover/cover.jpg -resize $2x$2 -gravity center -extent $2x$2 $opts $res/$1/ic_launcher.png
+}
+
+conv mipmap-xxhdpi 144
+conv mipmap-xhdpi 96
+conv mipmap-hdpi 72
+conv mipmap-mdpi 48
